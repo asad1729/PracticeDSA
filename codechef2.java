e7ebf1c9dcd57169a6ec27839b5589e9126ce03c
Asad Yousuf <71508128+asad1729@users.noreply.github.com>
2021-09-21 11:19:19 +0530

    Add files via upload

---
+++ codechef2.java
@@ -0,0 +1,23 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.stream.IntStream;
+
+
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+       Scanner in=new Scanner(System.in);
+       int t=in.nextInt();
+       for(int i=0;i<t;i++)
+       {
+           int n=in.nextInt();
+           int[] a = IntStream.range(1, 100).toArray();
+           
+
+       }
+    }
+}
