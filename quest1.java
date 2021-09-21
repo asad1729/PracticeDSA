572c018f3b13ea5be1d1f8572e51e53d3a8130e2
Asad Yousuf <71508128+asad1729@users.noreply.github.com>
2021-09-21 11:15:01 +0530

    Add files via upload

--- quest1.java
+++ quest1.java
@@ -1,15 +1,26 @@
-import java.io.IOException;
 import java.util.*;
 public class quest1 {
-    public static void main(String[] args)throws IOException
-    {
-        Scanner in=new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++)
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        int arr[]=new int[1000];
+        int p=0;
+        for (int j = 1; j < 1667; j++)
         {
-            String s=in.nextLine();
-
+            if (j % 3 == 0 || j % 10 == 3)
+                continue;
+            else
+            {
+                arr[p] = j;
+                p=p+1;
+            }
         }
+        for (int i = 0; i < t; i++)
+        {
+            int n = in.nextInt();
+            System.out.println(arr[n-1]);
+        }
+}}
 
-    }
-}
+
+
